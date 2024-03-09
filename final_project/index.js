6f305441c02d72e54a6ff75b313b5d556a348f0f
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-03-08 23:54:14 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-const jwtSecret = '244d0b97c61cb978567e348a15fc8cd5c3c5791af982ccae88db48383bc3c273';
 
 const app = express();
 
@@ -12,28 +11,25 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: "No token provided" });
-    }
-
-    const token = authHeader.split(' ')[1]; // Bearer <token>
-
-    // Verify the token
-    jwt.verify(token, jwtSecret, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: "Invalid token" });
+//Write the authenication mechanism here
+if(req.session.authorization) {
+    let token = req.session.authorization['accessToken']; // Access Token
+    jwt.verify(token, "access",(err,user)=>{
+        if(!err){
+            req.user = user;
+            next();
         }
-
-        // If token is valid, set the decoded user object to the request for use in other routes
-        req.user = decoded;
-        next(); // proceed to the next middleware or route handler
-    });
+        else{
+            return res.status(403).json({message: "User not authenticated"})
+        }
+     });
+ } else {
+     return res.status(403).json({message: "User not logged in"})
+ }
 });
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running on port 5000"));
