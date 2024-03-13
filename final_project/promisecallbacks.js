59fc25931d9537416fa56060672dbd3f1e4f2d50
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-03-12 19:14:19 -0500

    Update promisecallbacks.js

--- final_project/promisecallbacks.js
+++ final_project/promisecallbacks.js
@@ -10,40 +10,83 @@ public_users.get('/books', async function (req, res) {
   }
 });
 
-//TASK 11
-public_users.get('/isbn/:isbn', async function (req, res) {
-  try {
-    await new Promise((resolve) => {
-      resolve(res.send(JSON.stringify({ isbn }, null, 4)));
+// TASK 11
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[isbn]));
+    }
+        else {
+            reject(res.send('ISBN not found'));
+        }
     });
-    console.log("Promise for Task 10 resolved");
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+   }).
+        catch(function () { 
+                console.log('ISBN not found');
+  });
+
 });
 
-//TASK 12
-public_users.get('/author/:author', async function (req, res) {
-  try {
-    await new Promise((resolve) => {
-      resolve(res.send(JSON.stringify({ author }, { books }, null, 4)));
+
+// TASK 12
+public_users.get('/books/author/:author',function (req, res) {
+
+    const get_books_author = new Promise((resolve, reject) => {
+
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+      }
+
+
+    });
+    reject(res.send("The mentioned author does not exist "))
+
     });
 
-    console.log("Promise for Task 10 resolved");
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
-});
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
 
-//TASK 13
-public_users.get('/title/:title', async function (req, res) {
-  try {
-    await new Promise((resolve) => {
-      resolve(res.send(JSON.stringify({ title }, { books }, null, 4)));
+  });
+
+
+// TASK 13
+public_users.get('/books/title/:title',function (req, res) {
+
+    const get_books_title = new Promise((resolve, reject) => {
+
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "reviews":books[isbn]["reviews"]});
+    resolve(res.send(JSON.stringify({booksbytitle}, null, 4)));
+      }
     });
 
-    console.log("Promise for Task 10 resolved");
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
-});
+    reject(res.send("The mentioned title does not exist "))
+
+       });
+
+    get_books_title.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned book title doesnt exist');
+  });
+
+  });
